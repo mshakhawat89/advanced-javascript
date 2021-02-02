7b11a65adf63f5550d4d3e710057fcc270e26f2c
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-02-02 20:20:46 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+let pakhi;
+console.log(pakhi);
+
+
+
+function add(num1, num2){
+console.log(num1 +num2);
+return;
+}
+const result =  add(13, 25);
+console.log(result);
+undefined because there is no return;
+
+
+function add(num1, num2){
+    console.log(num1,num2);}
+    const result =  add(13);
+    console.log(result);
+
+
+
+const premik = {name:"smart dude", phone:54654,};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [55, 88, 99];
+console.log(ages[11]);
