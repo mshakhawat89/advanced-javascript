11965d211670caf734a9b248e3aacdd8167efea2
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-02-02 20:50:53 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,24 @@
+
+
+// const first = 2;
+// const second = "2";
+
+// const first = 2;
+// const second = true;
+
+// const first = 1;
+// const second = true;
+
+// const first = 1;
+// const second = false;
+
+const first = 1
+const second = true;
+
+if (first === second) {
+    console.log("condition is true");
+} 
+
+else {
+    console.log("condition is false");
+}
