5ba9d73cce375ba0d088dfee3afc7b70dc213502
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-02-02 19:51:59 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,29 @@
+
+
+// falsy ----
+
+// false;
+// 0;
+// "";
+// undefined;
+// null;
+// NaN;
+
+
+// truthy-----
+
+// "0";
+// " ";
+// [];
+
+
+
+
+
+let name =12 ;
+
+if (name || name ==0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
