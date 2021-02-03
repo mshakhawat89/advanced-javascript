48b37511329fae700feab7a76bcb071267b3a23e
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-02-03 11:33:08 +0600

    learned about break and continue

---
+++ break-continue.js
@@ -0,0 +1,26 @@
+// const nums = [1,2,3,4,5,6,7,8,9];
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+    // console.log(element);
+    // if (element>4) {
+    //     break;
+    // }
+
+
+      
+//     if (element>4) {
+//         break;
+//     }
+//     console.log(element);
+// }
+
+
+const nums = [1,2,-3,4,-5,6,7,-8,9];
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+  
+    if (element<0) {
+    continue;
+    }
+    console.log(element);
+}
