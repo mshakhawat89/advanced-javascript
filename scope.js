70e1f10d2fe324aca840b26eeb62c827d4e35e2a
md shakhawat hossain <hossainmdshakhawat19@gmail.com>
2021-02-03 08:46:39 +0600

    learning about scope

---
+++ scope.js
@@ -0,0 +1,42 @@
+
+// let bonus = 50;
+
+// function sum(first, second){
+//  let result = first + second + bonus;
+// //  console.log(bonus);
+// //  console.log(result);
+// return result;
+// }
+// // console.log(result);
+// console.log(bonus);
+// const output =sum(35,65);
+// console.log(output);
+
+
+
+
+
+
+
+
+// let bonus = 50;
+
+function sum(first, second){
+ let result = first + second;
+ if (result>90){
+var mood = "happy";
+mood = "funky";
+mood = "crunky";
+mood = "drunky";
+console.log(mood);
+ }
+ console.log(mood);
+//  console.log(bonus);
+//  console.log(result);
+return result;
+}
+
+// console.log(result);
+// console.log(bonus);
+const output =sum(35,65);
+// console.log(output);
